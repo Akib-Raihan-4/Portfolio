7fbf7ad076159b1c6a3392fcbb9de17daa56fbf4
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-08 19:31:55 -0400

    some changes to Project Mobile

--- components/indi-project.tsx
+++ components/indi-project.tsx
@@ -5,34 +5,23 @@ import Image from 'next/image'
 import { useScroll, motion, useTransform } from 'framer-motion'
 import React, { useRef } from 'react'
 import styles from './project.module.css'
+import { useMediaQuery } from 'react-responsive';
 
 type ProjectProps = (typeof projectsData)[number];
 
-
-const MobileProject = () =>{
-    return(
-        <div> </div>
-    )
-}
-
-
-
-export default function Project
-    ({
-        title,
-        description,
-        tags,
-        href,
-        imageUrl}:ProjectProps)
-        {
-            const ref = useRef<HTMLDivElement>(null)
-            const {scrollYProgress} = useScroll({
-                target: ref,
-                offset:["0 1", "1.33 1"]
-        }
-    ) 
-    
-
+export default function Project({
+    title,
+    description,
+    tags,
+    href,
+    imageUrl}:ProjectProps)
+    {
+        const ref = useRef<HTMLDivElement>(null)
+        const {scrollYProgress} = useScroll({
+            target: ref,
+            offset:["0 1", "1.33 1"]
+        })
+        const isMobile = useMediaQuery({ maxWidth: 640 });
     return(
         <motion.div 
          ref= {ref}
@@ -45,7 +34,7 @@ export default function Project
                     <div className={`${styles.imgBx}`}>
                         <Image src={imageUrl} alt={title} quality={95} className={`${styles.img}`}/>
                     </div>
-                    <div className={`${styles.content}`}>
+                    <div className={` ${isMobile ? styles.mobileContent : styles.content}`}>
                         <a href={href} target='blank'>  
                             <h2 className='font-extrabold text-[#220824] pb-3'>{title}</h2>
                         
